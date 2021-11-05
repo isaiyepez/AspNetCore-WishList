a97031c9df5e2206dad1c56fe8f4c46b76c3db32
Isai Yepez <isaiyepez@gmail.com>
2021-11-04 23:23:58 -0600

    Updated ItemController

--- WishList/Controllers/ItemController.cs
+++ WishList/Controllers/ItemController.cs
@@ -38,7 +38,8 @@ namespace WishList.Controllers
 
         public IActionResult Delete(int id)
         {
-            _context.Items.Remove(_context.Items.Find(id));
+            var item = _context.Items.FirstOrDefault(i => i.Id == id);
+            _context.Items.Remove(item);
             _context.SaveChanges();
             return RedirectToAction("Index");
         }
